525487ef795652438b0e72ee2906803462170bc2
Jiarong Wu <tg856416@login3.stampede2.tacc.utexas.edu>
2019-11-05 17:24:53 -0600

    Stampede2 directory cleaned and tested. Still need to sort out the turbulence source code from /tiger/forcing

--- cluster_spec/stampede2/turbulence/test1/produce.parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OUTFOLDER="$WORK/f$1"
-# Note that space is needed between = and []
-if [ "$2" = "clean" ]; then
-    rm -r $OUTFOLDER
-    echo "Directory cleaned!"
-fi
-mkdir $OUTFOLDER
-make -f Makefile.parallel OUTPUT=$1
