ea3b46248570e7073c9b7924bfe3005040d0cb4d
Jiarong Wu <jiarongw@della5.princeton.edu>
2021-05-06 14:35:39 -0400

    Add particle function test.

--- cluster_spec/della/linear/slurm_adaptive.della.sh
+++ cluster_spec/della/linear/slurm_adaptive.della.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 #SBATCH --mail-type=end
 #SBATCH --mail-user=jiarongw@princeton.edu
 
 # This is a slurm script for Della
-#The executable name
-EXE=wavewind_rerun
+# The executable name
+EXE=wavewind_rerun_test
 #Parameter value
-LEVEL=8
+LEVEL=11
 ak=0.05
-BO=1.47
+BO=200
 RE=10660. #Default 40000
 m=5
 B=0
-UstarRATIO=0.7
+UstarRATIO=1
 
 export ScratchDir="/scratch/gpfs/jiarongw/rerun/linear_${EXE}_Ustar${UstarRATIO}ak${ak}Bo${BO}Re${RE}LEVEL${LEVEL}"
 echo $ScratchDir
